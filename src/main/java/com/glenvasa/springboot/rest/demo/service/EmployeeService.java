d8f7f57d697fef9c58a473a53551b921672c3cc9
glenvasa <glenvasa@gmail.com>
2022-07-21 13:34:50 -0400

    Create EmployeeService.java

---
+++ src/main/java/com/glenvasa/springboot/rest/demo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.glenvasa.springboot.rest.demo.service;
+
+import com.glenvasa.springboot.rest.demo.model.Employee;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+}
