a92e5642ef83647a0c541bc6e00e0ff2fdfa8274
glenvasa <glenvasa@gmail.com>
2022-07-21 11:57:08 -0400

    Initial commit

---
+++ src/main/java/com/glenvasa/springboot/rest/demo/SpringbootRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.glenvasa.springboot.rest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glenvasa/springboot/rest/demo/SpringbootRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenvasa.springboot.rest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
