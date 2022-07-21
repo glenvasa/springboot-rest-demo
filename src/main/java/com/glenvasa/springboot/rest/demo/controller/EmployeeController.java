a7f9fbc246a8ffa3f304463b4fb0ac2a4c016b43
glenvasa <glenvasa@gmail.com>
2022-07-21 13:34:47 -0400

    Create EmployeeController.java

---
+++ src/main/java/com/glenvasa/springboot/rest/demo/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.glenvasa.springboot.rest.demo.controller;
+
+import com.glenvasa.springboot.rest.demo.model.Employee;
+import com.glenvasa.springboot.rest.demo.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Combines @Controller and @ResponseBody, so we don't need to annotate every method w/@ResponseBody
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // build create employee REST API
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+}
