c195971222e01ec4c1e39d785b2e45c2ba8312b7
glenvasa <glenvasa@gmail.com>
2022-07-21 14:36:07 -0400

    added getAllEmployees abstract method

--- src/main/java/com/glenvasa/springboot/rest/demo/service/EmployeeService.java
+++ src/main/java/com/glenvasa/springboot/rest/demo/service/EmployeeService.java
@@ -2,8 +2,11 @@ package com.glenvasa.springboot.rest.demo.service;
 
 import com.glenvasa.springboot.rest.demo.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
 
 }
